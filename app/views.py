dc5ad37731b5b03c96109ade2fe3e91e10f06bc5
kacperniwicki <s218325@student.uek.krakow.pl>
2021-05-25 18:14:37 +0200

    2021-05-25 KN

--- app/views.py
+++ app/views.py
@@ -1,22 +1,26 @@
 from app import app
+from app.models.product import Product
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
 
-@app.route('/extract')
-def extract():
-    pass
+@app.route('/extract/<product_id>')
+def extract(product_id):
+    product = Product(product_id)
+    product.extract_product()
+    product.save_to_json()
+    return str(product)
 
 @app.route('/products')
 def products():
     pass
 
-@app.route('/opinions/<productId>')
-def opinions(productId):
+@app.route('/opinions/<product_id>')
+def opinions(product_id):
     pass
 
 @app.route('/charts/<productId>')
-def charts(productId):
+def charts(product_id):
     pass
